f49f0727a93fde07eaba029867040adb823ecd97
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-06-14 13:27:32 +0200

    Add minimal command-line tool

--- distfeat/__main__.py
+++ distfeat/__main__.py
@@ -8,6 +8,7 @@ Module for command-line execution.
 
 # Import Python standard libraries
 import argparse
+import sys
 
 # Import our library
 import distfeat
@@ -18,9 +19,16 @@ def main():
     Main function for DistFeat call from command line.
     """
 
-    # parse command line arguments
+    if len(sys.argv) != 2:
+        print("DistFeat auxiliary query tool")
+        print("Usage: distfeat grapheme")
+        return
 
-    distfeat.dummy()
+    # build object
+    df = distfeat.DistFeat()
+
+    # print grapheme vector
+    print(df.grapheme2features(sys.argv[1]))
 
 
 if __name__ == "__main__":
