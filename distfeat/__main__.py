999a3f443fdb49db820bc8c593f5dedd157e84cf
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-06-23 09:06:38 +0200

    Add preliminary support for distances

--- distfeat/__main__.py
+++ distfeat/__main__.py
@@ -6,6 +6,8 @@ __main__.py
 Module for command-line execution.
 """
 
+# TODO: add grapheme2features options
+
 # Import Python standard libraries
 import argparse
 import sys
@@ -14,21 +16,50 @@ import sys
 import distfeat
 
 
+def parse_arguments():
+    # Create top-level parser
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # Create the parser for the "query" command
+    parser_query = subparsers.add_parser("query", help="Query the feature database")
+    parser_query.add_argument("grapheme", type=str, help="Grapheme to query")
+    parser_query.set_defaults(command="query")
+
+    # Create the parser for the "train" command
+    parser_distance = subparsers.add_parser(
+        "distance", help="Compute the distance between two graphemes"
+    )
+    parser_distance.add_argument(
+        "grapheme_a", type=str, help="First grapheme for distance quantification"
+    )
+    parser_distance.add_argument(
+        "grapheme_b", type=str, help="Second grapheme for distance quantification"
+    )
+    parser_distance.set_defaults(command="distance")
+
+    args = parser.parse_args()
+
+    return args
+
+
 def main():
     """
     Main function for DistFeat call from command line.
     """
 
-    if len(sys.argv) != 2:
-        print("DistFeat auxiliary query tool")
-        print("Usage: distfeat grapheme")
-        return
+    # Get arguments
+    args = parse_arguments()
 
-    # build object
+    # Act accordingly
     df = distfeat.DistFeat()
-
-    # print grapheme vector
-    print(df.grapheme2features(sys.argv[1]))
+    try:
+        if args.command == "query":
+            print(df.grapheme2features(args.grapheme))
+        elif args.command == "distance":
+            print(df.distance(args.grapheme_a, args.grapheme_b))
+    except AttributeError:
+        print("distfeat: error, must provide a command")
 
 
 if __name__ == "__main__":
--- distfeat/distfeat.py
+++ distfeat/distfeat.py
@@ -2,6 +2,9 @@
 Main module for the DistFeat library.
 """
 
+# TODO: allow to serialize regressors to disk
+# TODO: allow more options where training the regressor
+
 # Import Python libraries
 import csv
 from pathlib import Path
@@ -10,6 +13,14 @@ import unicodedata
 # Import 3rd party libraries
 import pyclts
 import tabulate
+
+# Try to import sklearn, which is not a dependency
+try:
+    from sklearn.neural_network import MLPRegressor
+
+    SKLEARN = True
+except:
+    SKLEARN = False
 
 
 def tabulate_matrix(matrix, tablefmt="simple"):
@@ -41,6 +52,39 @@ def tabulate_matrix(matrix, tablefmt="simple"):
     ]
 
     return tabulate.tabulate(data, headers=feature_list, tablefmt=tablefmt)
+
+
+def read_distance_matrix(filepath=None):
+    """
+    Read a distance matrix, used to seed a regressor.
+
+    Parameters
+    ==========
+    filepath : str
+        Path to the TSV file containing the distance matrix used to
+        seed the regressor. If not provided, will default to one derived
+        from data presented in Mielke (2012) and included in the library.
+
+    Returns
+    =======
+    matrix : dict
+        A dictionary of dictionaries with the distances as floating-point
+        values.
+    """
+
+    if not filepath:
+        filepath = Path(__file__).parent.parent / "resources" / "distances"
+        filepath = filepath / "mielke2012.tsv"
+        filepath = filepath.as_posix()
+
+    matrix = {}
+    with open(filepath) as tsvfile:
+        reader = csv.DictReader(tsvfile, delimiter="\t")
+        for row in reader:
+            grapheme = row.pop("GRAPHEME")
+            matrix[grapheme] = {gr: float(dist) for gr, dist in row.items()}
+
+    return matrix
 
 
 class DistFeat:
@@ -87,7 +131,9 @@ class DistFeat:
         # Build path, defaulting to `resources`
         file_path = f"{model}.tsv"
         if not path:
-            file_path = Path(__file__).parent.parent / "resources" / file_path
+            file_path = (
+                Path(__file__).parent.parent / "resources" / "models" / file_path
+            )
         else:
             file_path = Path(path) / file_path
 
@@ -116,7 +162,15 @@ class DistFeat:
                 if not self._features:
                     self._features = sorted(row.keys())
 
-    def grapheme2features(self, grapheme, t_values=True, vector=False):
+        # Instantiate a property for the regressor used for computing
+        # quantitative distances. All such methods require the `sklearn`
+        # library, which is *not* listed as a dependency; as such, by
+        # design we are not allowing to create it when initializing the
+        # object, and the user must be explicit about it.
+        # TODO: decide if we should allow to initialize
+        self._regressor = None
+
+    def grapheme2features(self, grapheme, mapper=None, t_values=True, vector=False):
         """
         Return the feature dictionary for a grapheme.
 
@@ -129,10 +183,14 @@ class DistFeat:
         grapheme : str
             The grapheme of the sound to be converted into a feature
             dictionary.
+        mapper : dict
+            A dictionary for mappin string values, intended for quantitative
+            usage. If provided, it takes precedence over `t_values`.
         t_values : bool
             A flag indicating whether the returned values should be
             mapped to their truth values (False/None/True) or not
-            (default: `True`).
+            (default: `True`). If provided, the `mapper` argument takes
+            precedence over this.
         vector : bool
             A flag indicating whether to return a vector of values as a
             list (default: `False`).
@@ -150,7 +208,12 @@ class DistFeat:
 
         # Extract and manipulate features
         features = self._model[grapheme]["features"]
-        if t_values:
+        if mapper:
+            features = {
+                feature_name: mapper[feature_val]
+                for feature_name, feature_val in features.items()
+            }
+        elif t_values:
             features = {
                 feature_name: self._tvalues[feature_val]
                 for feature_name, feature_val in features.items()
@@ -340,3 +403,98 @@ class DistFeat:
                         class_features[feature] = values[0]
 
         return class_features
+
+    def build_regressor(self, matrix_path=None):
+        """
+        Build or replace the quantitative distance regressor.
+
+        Note that this method, as all methods related to quantitative
+        distances, requires the `sklearn` library, which is not listed as
+        a dependency of the package.
+        """
+
+        # Make sure we have sklearn
+        if not SKLEARN:
+            raise RuntimeError("Distance methods require the `sklearn` library.")
+
+        # Read raw distance data and cache vectors, also allowing to
+        # skip over unmapped graphemes
+        raw_matrix = read_distance_matrix(matrix_path)
+        mapper = {"-": -1.0, "0": 0.0, "+": +1.0}
+        vector = {}
+        for grapheme in raw_matrix:
+            try:
+                vector[grapheme] = self.grapheme2features(
+                    grapheme, mapper=mapper, vector=True
+                )
+            except KeyError:
+                print("Skipping over unmapped [%s] grapheme..." % grapheme)
+
+        # Collect (X,y) vectors
+        X, y = [], []
+        for grapheme_a in raw_matrix:
+            # Skip over unmapped graphemes
+            if grapheme_a not in vector:
+                continue
+
+            for grapheme_b, dist in raw_matrix[grapheme_a].items():
+                # Skip over unmapped graphemes
+                if grapheme_b not in vector:
+                    continue
+
+                X.append(vector[grapheme_a] + vector[grapheme_b])
+                if dist == 0.0:
+                    y.append(dist)
+                else:
+                    y.append(dist + 1.0)
+
+        # Train regressor
+        print("Training MLPRegressor...")
+        self._regressor = MLPRegressor(random_state=1, max_iter=500)
+        self._regressor.fit(X, y)
+
+    def distance(self, grapheme_a, grapheme_b):
+        """
+        Return a quantitative distance based on a seed matrix.
+
+        The distance is by definition 0.0 for equal graphemes.
+
+        If no regressor has previously been trained, one will be trained with
+        default values and cached for future calls.
+
+        Note that this method, as all methods related to quantitative
+        distances, requires the `sklearn` library, which is not listed as
+        a dependency of the package.
+
+        Parameters
+        ==========
+        grapheme_a : str
+            The first grapheme to be used for distance computation.
+        grapheme_b : str
+            The second grapheme to be used for distance computation.
+
+        Returns
+        =======
+        dist : float
+            The distance between the two sounds.
+        """
+
+        # Make sure we have sklearn
+        if not SKLEARN:
+            raise RuntimeError("Distance methods require the `sklearn` library.")
+
+        # Build and cache a regressor with default parameters
+        if not self._regressor:
+            self.build_regressor()
+
+        # Get vectors
+        mapper = {"-": -1.0, "0": 0.0, "+": +1.0}
+        vector_a = self.grapheme2features(grapheme_a, mapper=mapper, vector=True)
+        vector_b = self.grapheme2features(grapheme_b, mapper=mapper, vector=True)
+
+        # If the vectors are equal, by definition the distance is zero
+        if tuple(vector_a) == tuple(vector_b):
+            return 0.0
+
+        # Compute distance with the regressor
+        return self._regressor.predict([vector_a + vector_b])[0]
